bdf761c94d4f4f6c42c42a41f5a0ac4429354559
rümeysa can <148216500+rumeysca@users.noreply.github.com>
2024-12-20 00:18:16 +0300

    Update main.c

--- cub3d/main.c
+++ cub3d/main.c
@@ -2,12 +2,18 @@
 
 int	init_map(t_map *cub)
 {
-	create_xpm(cub);
-	cub->mlx = mlx_init();
-	cub->mlx_win = mlx_new_window(cub->mlx, 1280, 720, "cub3D");
-	mlx_put_image_to_window(cub->mlx, cub->mlx_win, cub->floor, 32, 32);
-    mlx_loop(cub->mlx_win);
+	//create_xpm(cub);
+	//cub->mlx = mlx_init();
+	//cub->mlx_win = mlx_new_window(cub->mlx, 1280, 720, "cub3D");
+	//mlx_put_image_to_window(cub->mlx, cub->mlx_win, cub->floor, 32, 32);
+    //mlx_loop(cub->mlx_win);
 	return(0);
+}
+
+int ft_error(char *line)
+{
+	printf("Error %s\n", line);
+	return (1); // 1 mi 0 mı?
 }
 
 int	arg_checker(int argc, char *argv)
@@ -17,13 +23,13 @@ int	arg_checker(int argc, char *argv)
 	len = ft_strlen(argv);
 	if (argc != 2)
 	{
-		//ft_printf("Error : Invalid number of arguments!\n");
+		ft_error("Invalid argument count!");
 		return (1);
 	}
 	if (argv[len - 1] != 'b' && argv[len - 2] != 'u' && \
 			argv[len - 3] != 'c' && argv[len - 4] != '.')
 	{
-		//ft_printf("Error : File extension must be .cub!\n");
+		ft_error("File extension must be .cub!\n");
 		return (1);
 	}
 	return (0);
@@ -34,7 +40,8 @@ int	map_read_check(t_map *cub, char *f_name)
 	read_chars(cub, f_name);
 	if (!(cub->tmp_map))
 	{
-		//ft_printf("Error : Cannot read the map!\n");
+		free(cub->tmp_map);
+		ft_error("Cannot read the map!");
 		return (1);
 	}
 	return (0);
@@ -48,7 +55,10 @@ int	main(int argc, char **argv)
 	if (!cub)
 		return (1);
 	if (arg_checker(argc, argv[1]) || map_read_check(cub, argv[1]))
+	{
+		free (cub);
 		return (1);
+	}
 	if(!split_map(cub))
 		return (1);
 	//if (map_checker(cub))
