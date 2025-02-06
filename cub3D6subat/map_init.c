33d82b9940c4464be2ed4babd1be09a10dcbb9d6
rümeysa can <148216500+rumeysca@users.noreply.github.com>
2025-02-07 01:51:01 +0300

    Add files via upload
    etrafı duvarlarla çevrili

---
+++ cub3D6subat/map_init.c
@@ -0,0 +1,222 @@
+#include "cubthreed.h"
+
+int handle_color(t_map *cub)
+{
+	int i;
+	int ret;
+
+	ret = 1;
+	i = 1;
+	while (cub->tmp_map[cub->index_floor][i] != '\n')
+	{
+		ret = rgb_check(cub->tmp_map[cub->index_floor][i]); // 0 ise hatalı
+		if(ret == 0)
+			return (ret);
+		i++;
+	}
+	i = 1;
+	while (cub->tmp_map[cub->index_sky][i] != '\n')
+	{
+		ret = rgb_check(cub->tmp_map[cub->index_sky][i]);
+		if(ret == 0)
+			return (ret);
+		i++;
+	}
+	if(ret && set_color(cub))
+	{
+		ft_error("Color error");
+		cub_free(cub);
+		exit (1);
+	}
+	return (ret); // 0 ise hatalı
+}
+
+int rgb_check(char a)
+{
+    char *str;
+    int i;
+
+    i = 0;
+    str = "0123456789, 	";
+    while(str[i])
+    {
+        if(str[i] != a)
+            i++;
+        else
+            return (1);
+    }
+    return (0);
+}
+
+int coma_count(t_map *cub)
+{
+	int i;
+	int coma;
+
+	i = 0;
+	coma = 0;
+	while(cub->tmp_map[cub->index_sky][i])
+	{
+		if(cub->tmp_map[cub->index_sky][i] == ',')
+			coma++;
+		i++;
+	}
+	if (coma != 2)
+		return (1);
+	i = 0;
+	coma = 0;
+	while(cub->tmp_map[cub->index_floor][i])
+	{
+		if(cub->tmp_map[cub->index_floor][i] == ',')
+			coma++;
+		i++;
+	}
+	if (coma != 2)
+		return (1);
+	return (0);
+}
+
+int set_color(t_map* cub)
+{
+	char *tmp;
+	int i;
+
+	if(coma_count(cub))
+		return (1);
+	tmp = ft_strtrim(cub->tmp_map[cub->index_floor] + 1, " \n");
+	if(ft_strchr(tmp , ' '))
+	{
+		free(tmp);
+		return (1);
+	}
+	cub->floor_char = ft_split(tmp, ',');
+	i = 0;
+	while(cub->floor_char[i])
+		i++;
+	if(i != 3)
+	{
+		free(tmp);
+		ft_free_array(cub->floor_char);
+		return (1);
+	}
+	free(tmp);
+	tmp = ft_strtrim(cub->tmp_map[cub->index_sky] + 1, " \n");
+	if(ft_strchr(tmp , ' '))
+	{
+		free(tmp);
+		ft_free_array(cub->floor_char);
+		return (1);
+	}
+	cub->sky_char = ft_split(tmp, ',');
+	i = 0;
+	while(cub->sky_char[i])
+		i++;
+	if(i != 3)
+	{
+		free(tmp);
+		ft_free_array(cub->floor_char);
+		ft_free_array(cub->sky_char);
+		return (1);
+	}
+	free(tmp);
+	color_checker(cub);
+	return (0);
+}
+
+void color_checker(t_map *cub) // rgb aralığı
+{
+	int i;
+
+	i = 0;
+	while(cub->floor_char[i])
+	{
+		if(ft_strlen(cub->floor_char[i]) == 0 || ft_strlen(cub->floor_char[i]) > 3)
+		{
+			ft_free_color(cub);
+			ft_error("Invalid RGB");
+			exit (1);
+		}
+		i++;
+	}
+	while(cub->sky_char[i])
+	{
+		if(ft_strlen(cub->sky_char[i]) == 0 || ft_strlen(cub->sky_char[i]) > 3)
+		{
+			ft_free_color(cub);
+			ft_error("Invalid RGB");
+			exit (1);
+		}
+		i++;
+	}
+	color_atoi(cub);
+}
+
+void	color_atoi(t_map *cub)
+{
+	int i;
+
+	i = 0;
+	cub->col_floor = (int *)malloc(3 * sizeof(int));
+	cub->col_sky = (int *)malloc(3 * sizeof(int)); // burası doğru mu
+	while(cub->sky_char[i])
+	{
+		cub->col_sky[i] = ft_atoi(cub->sky_char[i]);
+		i++;
+	}
+	i = 0;
+	while(cub->floor_char[i])
+	{
+		cub->col_floor[i] = ft_atoi(cub->floor_char[i]);
+		i++;
+	}
+	if(color_range(cub))
+	{
+		free(cub->col_floor);
+		free(cub->col_sky);
+		ft_free_color(cub);
+		ft_error("Color range!");
+		exit (0);
+	}
+}
+
+int	color_range(t_map *cub)
+{
+	int i;
+
+	i = 0;	
+	while(i < 3)
+	{
+		if(cub->col_sky[i] > 255 || cub->col_sky[i] < 0)
+			return (1);
+		i++;
+	}
+	i = 0;
+	while(i < 3)
+	{
+		if(cub->col_floor[i] > 255 || cub->col_floor[i] < 0)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+void handle_texture(t_map* cub)// dosya var mı kontrolü eklencek/ open close
+{
+	char *ea;
+	char *no;
+	char *so;
+	char *we;
+
+	ea = ft_strtrim((cub->tmp_map[cub->index_ea] + 2), " ");
+	no = ft_strtrim((cub->tmp_map[cub->index_no] + 2), " ");
+	so = ft_strtrim((cub->tmp_map[cub->index_so] + 2), " ");
+	we = ft_strtrim((cub->tmp_map[cub->index_we] + 2), " ");
+	cub->tex_ea = ft_strtrim((ea), "\n");
+	cub->tex_no = ft_strtrim((no), "\n");
+	cub->tex_so = ft_strtrim((so), "\n");
+	cub->tex_we = ft_strtrim((we), "\n");
+	free(ea);
+	free(no);
+	free(so);
+	free(we);
+}
---
+++ cub3D6subat/ray_casting.c
@@ -0,0 +1,62 @@
+#include "cubthreed.h"
+
+void	calc_side(t_map *cub)
+{
+	if (cub->raycast->raydir_x < 0)
+	{
+		cub->raycast->step_x = -1;
+		cub->raycast->sidedist_x = (cub->player->x - cub->loc_px) \
+			* cub->raycast->deltadist_x;
+	}
+	else
+	{
+		cub->raycast->step_x = 1;
+		cub->raycast->sidedist_x = (cub->loc_px + 1.0 - cub->player->x) \
+			* cub->raycast->deltadist_x;
+	}
+	if (cub->raycast->raydir_y < 0)
+	{
+		cub->raycast->step_y = -1;
+		cub->raycast->sidedist_y = (cub->player->y - cub->loc_py) \
+			* cub->raycast->deltadist_y;
+	}
+	else
+	{
+		cub->raycast->step_y = 1;
+		cub->raycast->sidedist_y = (cub->loc_py + 1.0 - cub->player->y) \
+			* cub->raycast->deltadist_y;
+	}
+}
+
+void	calc_ray(t_map *cub, int x)
+{
+	init_raycast(cub, x);
+	calc_side(cub);
+}
+
+void	dda(t_map *cub)
+{
+	while (cub->raycast->hit == 0)
+	{
+		if (cub->raycast->sidedist_x < cub->raycast->sidedist_y)
+		{
+			cub->raycast->sidedist_x += cub->raycast->deltadist_x;
+			cub->loc_px += cub->raycast->step_x;
+			if (cub->raycast->step_x == 1)
+				cub->raycast->side1 = 1;
+			else
+				cub->raycast->side1 = 0;
+		}
+		else
+		{
+			cub->raycast->sidedist_y += cub->raycast->deltadist_y;
+			cub->loc_py += cub->raycast->step_y;
+			if (cub->raycast->step_y == 1)
+				cub->raycast->side1 = 2;
+			else
+				cub->raycast->side1 = 3;
+		}
+		if (cub->map_game[cub->loc_py][cub->loc_px] == '1')
+			cub->raycast->hit = 1;
+	}
+}
